50875e53e59bf6a8306443a43b3a2e33950bc7bb
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 <wbo1026@naver.com>
2023-02-20 20:42:29 +0900

    SSAFY Algorithm_Study 4th - 우수인 직사각형으로 나누기 수정

--- week4/wbo1026/Boj1451_직사각형으로나누기.java
+++ week4/wbo1026/Boj1451_직사각형으로나누기.java
@@ -4,20 +4,19 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Boj1451 {
+public class Boj1451{
     static int N, M;
     static long max;
     static int[][] rec;
+    static long[][] sum;
 
     static void rec1() { // III 케이스
         for (int i = 1; i <= M - 2; i++) {
             for (int j = i + 1; j <= M - 1; j++) {
-                long a = sum(1, 1, N, i);
-                long b = sum(1, i + 1, N, j);
-                long c = sum(1, j + 1, N, M);
-                if (max < a * b * c) {
-                    max = a * b * c;
-                }
+                long a = sum2(1, 1, N, i);
+                long b = sum2(1, i + 1, N, j);
+                long c = sum2(1, j + 1, N, M);
+                max = Math.max(max, a * b * c);
             }
         }
     }
@@ -25,12 +24,10 @@ public class Boj1451 {
     static void rec2() { // 三 케이스
         for (int i = 1; i <= N - 2; i++) {
             for (int j = i + 1; j <= N - 1; j++) {
-                long a = sum(1, 1, i, M);
-                long b = sum(i + 1, 1, j, M);
-                long c = sum(j + 1, 1, N, M);
-                if (max < a * b * c) {
-                    max = a * b * c;
-                }
+                long a = sum2(1, 1, i, M);
+                long b = sum2(i + 1, 1, j, M);
+                long c = sum2(j + 1, 1, N, M);
+                max = Math.max(max, a * b * c);
             }
         }
     }
@@ -38,12 +35,10 @@ public class Boj1451 {
     static void rec3() { // ㅑ 케이스
         for (int i = 1; i <= N - 1; i++) {
             for (int j = 1; j <= M - 1; j++) {
-                long a = sum(1, 1, N, j);
-                long b = sum(1, j + 1, i, M);
-                long c = sum(i + 1, j + 1, N, M);
-                if (max < a * b * c) {
-                    max = a * b * c;
-                }
+                long a = sum2(1, 1, N, j);
+                long b = sum2(1, j + 1, i, M);
+                long c = sum2(i + 1, j + 1, N, M);
+                max = Math.max(max, a * b * c);
             }
         }
     }
@@ -51,12 +46,10 @@ public class Boj1451 {
     static void rec4() { // ㅕ 케이스
         for (int i = 1; i <= N - 1; i++) {
             for (int j = 1; j <= M - 1; j++) {
-                long a = sum(1, 1, i, j);
-                long b = sum(i + 1, 1, N, j);
-                long c = sum(1, j + 1, N, M);
-                if (max < a * b * c) {
-                    max = a * b * c;
-                }
+                long a = sum2(1, 1, i, j);
+                long b = sum2(i + 1, 1, N, j);
+                long c = sum2(1, j + 1, N, M);
+                max = Math.max(max, a * b * c);
             }
         }
     }
@@ -64,12 +57,10 @@ public class Boj1451 {
     static void rec5() { // ㅠ 케이스
         for (int i = 1; i <= N - 1; i++) {
             for (int j = 1; j <= M - 1; j++) {
-                long a = sum(1, 1, i, M);
-                long b = sum(i + 1, 1, N, j);
-                long c = sum(i + 1, j + 1, N, M);
-                if (max < a * b * c) {
-                    max = a * b * c;
-                }
+                long a = sum2(1, 1, i, M);
+                long b = sum2(i + 1, 1, N, j);
+                long c = sum2(i + 1, j + 1, N, M);
+                max = Math.max(max, a * b * c);
             }
         }
     }
@@ -77,17 +68,16 @@ public class Boj1451 {
     static void rec6() { // ㅛ 케이스
         for (int i = 1; i <= N - 1; i++) {
             for (int j = 1; j <= M - 1; j++) {
-                long a = sum(1, 1, i, j);
-                long b = sum(1, j + 1, i, M);
-                long c = sum(i + 1, 1, N, M);
-                if (max < a * b * c) {
-                    max = a * b * c;
-                }
+                long a = sum2(1, 1, i, j);
+                long b = sum2(1, j + 1, i, M);
+                long c = sum2(i + 1, 1, N, M);
+                max = Math.max(max, a * b * c);
             }
         }
     }
 
-    static long sum(int startY, int startX, int endY, int endX) { // 합 구하기
+    // 1. for문으로 합 구하기
+    static long sum(int startY, int startX, int endY, int endX) {
         long sum = 0;
 
         for (int i = startY; i <= endY; i++) {
@@ -95,8 +85,14 @@ public class Boj1451 {
                 sum += rec[i][j];
             }
         }
-
         return sum;
+    }
+
+    // 2. 누적합을 이용해 구하기
+    static long sum2(int startY, int startX, int endY, int endX) {
+
+        return sum[endY][endX] - sum[endY][startX - 1] - sum[startY - 1][endX] + rec[startY - 1][startX - 1];
+
     }
 
     public static void main(String[] args) throws Exception {
@@ -106,6 +102,7 @@ public class Boj1451 {
         M = Integer.parseInt(st.nextToken()); // 가로
 
         rec = new int[N + 1][M + 1];
+        sum = new long[N + 1][M + 1];
 
         for (int i = 1; i <= N; i++) {
             String str = br.readLine();
@@ -114,7 +111,14 @@ public class Boj1451 {
             }
         }
 
+        for (int i = 1; i <= N; i++) { // 누적합 구하기
+            for (int j = 1; j <= M; j++) {
+                sum[i][j] = sum[i][j - 1] + sum[i - 1][j] + (long) rec[i][j] - sum[i - 1][j - 1];
+            }
+        }
+
         // end input
+
         rec1();
         rec2();
         rec3();
