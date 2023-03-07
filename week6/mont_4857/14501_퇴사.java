c6faab0344404c8b5350197a458f792d26c64439
KyuDonLim <87511294+KyuDonLim@users.noreply.github.com>
2023-03-07 17:05:41 +0900

    Update 14501_퇴사.java

--- week6/mont_4857/14501_퇴사.java
+++ week6/mont_4857/14501_퇴사.java
@@ -11,11 +11,11 @@ public class Main {
 		
 		StringTokenizer st;
     
-    // 끝나는 일을 기준으로 정렬하기 위한 PriorityQueue
+    		// 끝나는 일을 기준으로 정렬하기 위한 PriorityQueue
 		PriorityQueue<Work> pq = new PriorityQueue<>();
 		int[] day = new int[N+1];
 		
-    // 모든 일을 pq에 저장
+    		// 모든 일을 pq에 저장
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int end = Integer.parseInt(st.nextToken());
@@ -23,10 +23,17 @@ public class Main {
 			pq.offer(new Work((i+1),end+i,pay));
 		}
     
-    // 0번째 날 초기화
+   		 // 0번째 날 초기화
 		day[0] = 0;
 		
-    //
+		/*
+		 * 1. 해당 날짜별로 최대값 비교
+		 * 2. 해당 날짜에 일이 있는 지 확인
+		 * 2-1. 일이 존재하는 경우
+		         -> 해당 날짜의 최대값 = Math.max(전날의 최대값, 해당 일의 시작 전날의 최대값 + 해당 일의 가격)
+	   	 * 2-2. 일이 없는 경우
+		         -> 전날의 최대값을 입력
+		*/
 		for (int i = 1; i < day.length; i++) {
 			boolean isT = false;
 			while(!pq.isEmpty() && pq.peek().end == i) {
@@ -54,10 +61,10 @@ public class Main {
 			return "Work [start=" + start + ", end=" + end + ", pay=" + pay + "]";
 		}
     
-    /*
-     * 1. 끝나는 날이 빠른 것
-     * 2. 시작 날이 느린 것
-    */
+  		/*
+  		 * 1. 끝나는 날이 빠른 것
+  		 * 2. 시작 날이 느린 것
+   		*/
 		@Override
 		public int compareTo(Work o) {
 			if(this.end == o.end) {
